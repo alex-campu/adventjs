1293f3ef713eecfb0429e18a88a040f260431290
alex-campu <alex.campuzano316@gmail.com>
2021-12-22 11:23:44 -0700

    day 1 soluton

---
+++ day1/ovejas.js
@@ -0,0 +1,37 @@
+/*
+https://adventjs.dev/challenges/01
+
+Considera una lista/array de ovejas. Cada oveja tiene un nombre y un color.
+Haz una función que devuelva una lista con todas las ovejas que sean de color
+rojo y que además su nombre contenga tanto las letras n Y a, sin importar 
+el orden, las mayúsculas o espacios.
+*/
+
+const ovejas = [
+  { name: "Noa", color: "azul" },
+  { name: "Euge", color: "rojo" },
+  { name: "Navidad", color: "rojo" },
+  { name: "Ki Na Ma", color: "rojo" },
+  { name: "AAAAAaaaaa", color: "rojo" },
+  { name: "Nnnnnnnn", color: "rojo" },
+];
+
+function contarOvejas(ovejas) {
+  // aquí tu magia
+
+  const redItems = ovejas.filter((item) => item.color === "rojo");
+
+  const includesA = redItems.filter((item) =>
+    item.name.toUpperCase().includes("A")
+  );
+
+  const includesN = includesA.filter((item) =>
+    item.name.toUpperCase().includes("N")
+  );
+
+  return includesN;
+}
+
+const ovejasFiltradas = contarOvejas(ovejas);
+
+console.log(ovejasFiltradas);
